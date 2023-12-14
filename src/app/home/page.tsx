cd733859e5a3d0505c98a2b4b950b8d7b25302c8
Selasie5 <selasisepenu5@gmail.com>
2023-12-14 16:55:58 +0000

    hero section complete

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,16 @@
 import styles from "../home.module.css"
+import Link from "next/link"
 const home = () => {
   return (
     <section className={`flex flex-col justify-center px-10 ${styles.header}`}>
       <div className="w-3/4">
         <h1 className="text-white font-bold text-8xl">Purely <span className="text-green-600">Ghanaian</span> - <span className="text-green-600">Natural </span> Bitters Essence</h1>
       </div>
+     <div>
+      <Link href="">
+        <button className="bg-white w-48 h-12 text-green-600 font-medium rounded-md">Learn More</button>
+      </Link>
+     </div>
     </section>
   )
 }
